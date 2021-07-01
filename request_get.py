948084e161f11847fe21c5e00f0fbdbbc1e358d1
Vane79 <alehin.vane@gmail.com>
2021-07-01 18:30:38 +0700

    Made urls into variables

--- request_get.py
+++ request_get.py
@@ -15,7 +15,7 @@ def test_func_get_success_200(var):
         print("Different exit code on GET, test GET for exit code 400 *FAILED*")
 
 
-def test_func_get_failure_400(var):
+def test_func_get_failure_400(var):  # пытаюсь ввести неверный аттрибут для поиска, возвращает 200.
     try:
         response = requests.get(var, params=inv_value)
         assert response.status_code == 400
